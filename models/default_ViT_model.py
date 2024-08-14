9dac4ce215ddc43cebf28a5f745e4e32a987386b
Taewoo Kim <skitwau@gmail.com>
2024-08-14 15:17:20 +0900

    모델 생성 코드

--- models/default_ViT_model.py
+++ models/default_ViT_model.py
@@ -3,9 +3,9 @@ import torch
 import torch.nn as nn
 
 class BaseModel(nn.Module):
-    def __init__(self, model_name, pretrained, num_classes):
+    def __init__(self, model_name, pretrained, num_classes, in_chans):
         super(BaseModel, self).__init__()
-        self.base_model = timm.create_model(model_name=model_name, pretrained=pretrained, num_classes=num_classes)
+        self.base_model = timm.create_model(model_name=model_name, pretrained=pretrained, num_classes=num_classes, in_chans=in_chans)
 
     def forward(self, x):
         x = self.base_model(x)
@@ -20,7 +20,7 @@ class MLP_layer(nn.Module):
         self.num_features = self.base_model.num_features
 
         # 마블링, 조직감, 기호도를 위한 MLP
-        self.marbling_texture_preference = nn.Sequential(
+        self.marbling_texture_total = nn.Sequential(
             nn.Linear(self.num_features, 256),
             nn.ReLU(),
             nn.Linear(256, 3)
@@ -45,15 +45,26 @@ class MLP_layer(nn.Module):
         
         features = base_output
 
-        marbling_texture_preference = self.marbling_texture_preference(features)
+        marbling_texture_total = self.marbling_texture_total(features)
         moisture = self.moisture_head(features)
         color = self.color_head(features)
         
         # 원래의 라벨 순서대로 출력을 결합
         return torch.cat([
-            marbling_texture_preference[:, 0:1],  # 마블링
+            marbling_texture_total[:, 0:1],  # 마블링
             color,  # 색깔
-            marbling_texture_preference[:, 1:2],  # 조직감
+            marbling_texture_total[:, 1:2],  # 조직감
             moisture,  # 표면육즙
-            marbling_texture_preference[:, 2:3]  # 기호도
+            marbling_texture_total[:, 2:3]  # 기호도
         ], dim=1)
+    
+
+def create_model(model_name, pretrained, num_classes, in_chans, out_dim):
+
+    if out_dim != 5:
+        raise ValueError("out_dim must be exactly 5 for this model")
+
+    base_model = BaseModel(model_name, pretrained, num_classes, in_chans)
+    model = MLP_layer(base_model)
+
+    return model
---
+++ models/model.py
@@ -0,0 +1,33 @@
+import torch
+import importlib
+import mlflow
+import mlflow.pyfunc
+
+def make_model(config):
+
+    model_cfg = config['models']
+
+    model_name = model_cfg['model_name']
+    model_file = model_cfg['model_file']
+    load_model = model_cfg['load_model']
+    model_version = model_cfg['model_version']
+    num_classes = model_cfg['num_classes']
+    out_dim = model_cfg['out_dim']
+    pretrained = model_cfg['pretrained']
+    in_chans = model_cfg['in_chans']
+
+    if not load_model :
+        module = importlib.import_module(model_file)
+        model = module.create_model(model_name, pretrained, num_classes, in_chans, out_dim)
+    
+    else :
+        mlflow.set_tracking_uri("uri 설정")
+        model_uri = f"models:/{model_name}/{model_version}"
+
+        try:
+            model = mlflow.pytorch.load_model(model_uri)
+            print(f"Success to load model")
+        except mlflow.exceptions.MlflowException as e:
+            print(f"Error loading model: {e}")
+    
+    return model
