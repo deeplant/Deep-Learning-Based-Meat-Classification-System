6f15bc712588c37cf2a48d124a7ef3e91cc80742
Taewoo Kim <skitwau@gmail.com>
2024-09-18 16:26:40 +0900

    out dim 수정

--- models/default_model.py
+++ models/default_model.py
@@ -19,37 +19,30 @@ class MLP_layer(nn.Module):
         self.base_model = base_model
         self.out_dim = out_dim
 
-        if out_dim > 0:
-            self.num_features = self.base_model.base_model.num_features
-            
-            # out_dim 개수만큼 MLP 생성
-            self.mlp_heads = nn.ModuleList([
-                nn.Sequential(
-                    nn.Linear(self.num_features, 64),
-                    nn.ReLU(),
-                    nn.Linear(64, 1)
-                ) for _ in range(out_dim)
-            ])
-        else:
-            self.mlp_heads = None
+        self.num_features = self.base_model.base_model.num_features
+        
+        # out_dim 개수만큼 MLP 생성
+        self.mlp_heads = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(self.num_features, 64),
+                nn.ReLU(),
+                nn.Linear(64, 1)
+            ) for _ in range(out_dim)
+        ])
 
     def forward(self, x):
         base_output = self.base_model(x)
         
-        if self.out_dim > 0:
-            features = base_output
+        features = base_output
 
-            outputs = [head(features) for head in self.mlp_heads]
-            return torch.cat(outputs, dim=1)
-        else:
-            
-            return base_output
-    
+        outputs = [head(features) for head in self.mlp_heads]
+
+        return torch.cat(outputs, dim=1)
 
 def create_model(model_name, pretrained, num_classes, in_chans, out_dim):
 
     if out_dim <= 0:
-        print("분류 작업 진행: out_dim이 0 이하입니다.")
+        raise ValueError("오류: out_dim이 0 이하입니다.")
 
     base_model = BaseModel(model_name, pretrained, num_classes, in_chans)
     model = MLP_layer(base_model, out_dim)
